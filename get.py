5ad5a2181bb8f7effd5bd4e3ed3bdbc10cbaba86
Zaven96 <46746797+Zaven96@users.noreply.github.com>
2019-04-11 16:21:04 +0400

    Add files via upload

---
+++ get.py
@@ -0,0 +1,10 @@
+import urllib.request
+value = input('Enter your base : ')
+content = urllib.request.urlopen("https://api.exchangeratesapi.io/latest?base=" + value)
+for line in content:
+    kaxamb = line.decode('utf-8')
+    varung = kaxamb.replace(',', '\n')
+    print(varung)
+
+
+
